ac35023cbe15a80a27e59be0c3d50e47caabd4c7
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2024-07-01 11:09:38 +0700

    remove unused

--- config.go
+++ config.go
@@ -1,10 +1,7 @@
 package main
 
-import "fmt"
-
 var (
 	AppVersions = [3]uint32{0, 1, 0}
-	AppVersion  = fmt.Sprintf("v%d.%d.%d", AppVersions[0], AppVersions[1], AppVersions[2])
 	AppPort     = "4001"
 	AppOs       = "GowaAPI"
 )
